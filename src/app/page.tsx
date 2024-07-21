e28c4426f2dae8f3445dccc4e0ac231eb07c5681
[jutrong] <[user email]>
2024-07-21 23:02:22 +0900

    Feat: markdown 불러오기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import Header from "@/components/Header";
-import Image from "next/image";
+import { getSortedPostsData } from "@/lib/posts";
 
 export default function Home() {
+  const posts = getSortedPostsData();
+
   return (
     <main className="w-screen h-full">
       <Header />
+      <div>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <p>{post.title}</p>
+            <p>{post.date}</p>
+          </div>
+        ))}
+      </div>
     </main >
   );
 }
