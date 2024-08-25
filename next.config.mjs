262e9e2c41a38de01d6dd9b786032124042dd19a
jutrong <leek71358@gmail.com>
2024-08-25 23:21:12 +0900

    Chore: ignore lint build

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ import rehypePrism from "rehype-prism-plus";
 const nextConfig = {
   // 마크다운 및 MDX 파일을 포함시키기 위해 페이지 확장자 설정
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 const options = {
